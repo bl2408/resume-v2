8cdeb28b58ca60f33f397631fb7e28b5ceb69e7a
Brian Lambert <brianlambert2408@gmail.com>
2023-08-08 18:03:49 +1000

    Extended screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = import("tailwindcss/defaultTheme");
 
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		screens: {
-			"2xs": "360px",
-			xs: "475px",
-			sm2: "550px",
-			...defaultTheme.screens,
-		},
 		extend: {
 			fontFamily: {
 				encodeSans: ["Encode Sans", "sans-serif"],
+			},
+			screens: {
+				"2xs": "360px",
+				xs: "475px",
+				sm2: "550px",
 			},
 		},
 	},
