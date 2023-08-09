f901350fc475d47878d774c0fc7984099de567f0
Brian Lambert <brianlambert2408@gmail.com>
2023-08-09 12:46:43 +1000

    Added education section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import Content from "./Components/Content";
 import HardSkills from "./Components/HardSkills";
 import Splash from "./Components/Splash";
 import expContent from "./Content/experience.json";
+import eduContent from "./Content/education.json";
 
 export default function App() {
 	return (
 		<main className="p-10 flex items-center gap-10 flex-col [&>*]:max-w-screen-md2 [&>*]:w-full min-w-[320px]">
 			<Splash />
 			<HardSkills />
-			<Content h1="Experience" content={expContent} />
+			<Content h1="Experience" content={expContent} limit={3} />
+			<Content h1="Education" content={eduContent} />
 		</main>
 	);
 }
