f54e6012634a3a31b67cda662735059e5c8d0d8f
Brian Lambert <brianlambert2408@gmail.com>
2023-08-10 13:01:24 +1000

    Added GH pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes("node_modules")) {
-						return id
-							.toString()
-							.split("node_modules/")[1]
-							.split("/")[0]
-							.toString();
-					}
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	console.log(
+		"USING BASE: " + (env.BUILD_BASE === "ghpages" ? "/resume-v2/" : "/")
+	);
+	return {
+		base: env.BUILD_BASE === "ghpages" ? "/resume-v2/" : "/",
+		plugins: [react()],
+		build: {
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("node_modules")) {
+							return id
+								.toString()
+								.split("node_modules/")[1]
+								.split("/")[0]
+								.toString();
+						}
+					},
 				},
 			},
 		},
-	},
+	};
 });
